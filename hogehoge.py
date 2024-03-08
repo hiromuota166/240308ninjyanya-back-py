c6fec9d9fd58b5cf9c9e35d957a535979f761149
hiromuota166 <otahiromu166@gmail.com>
2024-03-09 00:22:25 +0900

    ゴミバックエンドをつくるぞー！！

---
+++ hogehoge.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+stream = client.chat.completions.create(
+    model="gpt-4",
+    messages=[{"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
